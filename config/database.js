d660c267f46c3bbea6f68189d7d52d20b1f7ff45
Juan Pérez <125818073+juanperezzdp@users.noreply.github.com>
2023-07-12 19:15:38 -0500

    Aida

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Uri_DB = 'mongodb://localhost:27017/MERNcrud';
+
+const connect = async () => {
+  try {
+    await mongoose.connect(Uri_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión establecida a MongoDB');
+  } catch (err) {
+    console.error('Error de conexión a MongoDB:', err);
+  }
+};
+
+module.exports = connect;
